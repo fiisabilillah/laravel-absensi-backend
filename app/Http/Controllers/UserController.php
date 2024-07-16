aec1c0ad94a50fbeb63fc3e28d78d36fb5af6f2c
fiisabilillah <officialteam548@gmail.com>
2024-07-16 08:27:51 +0700

    fortify, create, edit, deleted

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        // $users = \App\Models\User::paginate(10);
+        $users = DB::table('users')
+            ->when($request->input('name'), function ($query, $name) {
+                return $query->where('name', 'like', '%' . $name . '%');
+            })
+            ->orderBy('id', 'desc')
+            ->paginate(10);
+        return view('pages.users.index', compact('users'));
+    }
+
+    public function create()
+    {
+        return view('pages.users.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $data = $request->all();
+        $data['password'] = Hash::make($request->password);
+        \App\Models\User::create($data);
+        return redirect()->route('users.index')->with('success', 'User successfully created');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    //edit
+    public function edit(User $user)
+    {
+        return view('pages.users.edit', compact('user'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    //update
+    public function update(Request $request, User $user)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+        ]);
+
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'role' => $request->role,
+            'position' => $request->position,
+            'department' => $request->department,
+        ]);
+
+        //if password filled
+        if ($request->password) {
+            $user->update([
+                'password' => Hash::make($request->password),
+            ]);
+        }
+
+        return redirect()->route('users.index')->with('success', 'User updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('users.index')->with('success', 'User successfully deleted');
+    }
+}
