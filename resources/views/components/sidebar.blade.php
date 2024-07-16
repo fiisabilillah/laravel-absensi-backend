8007ede69820dcadd46c9c10d9bfc26e7441b558
fiisabilillah <officialteam548@gmail.com>
2024-07-17 04:39:04 +0700

    company dashboard and company

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,27 +1,28 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="index.html">CBT TPA</a>
+            <a href="index.html">BAHRI HR</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="index.html">St</a>
         </div>
         <ul class="sidebar-menu">
-            <li class="menu-header">Dashboard</li>
-            <li class="nav-item dropdown">
-                <a href="{{route('users.index')}}" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
-                <ul class="dropdown-menu">
 
-                    <li class="nav-item ">
-                        <a href="" class="nav-link "><i class="fas fa-columns"></i>
-                            <span>Users</span></a>
-                    </li>
-                </ul>
+            <li class="nav-item  ">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+
             </li>
-    </aside>
-        <ul class="sidebar-menu">
-            <li class="nav-item dropdown">
-                <a href="{{route('users.index')}}" class="nav-link"><i class="fas fa-fire"></i><span>Users</span></a>
+
+            <li class="nav-item ">
+                <a href="{{ route('users.index') }}" class="nav-link "><i class="fas fa-columns"></i>
+                    <span>Users</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a href="{{ route('companies.show', 1) }}" class="nav-link">
+                    <i class="fas fa-columns"></i>
+                    <span>Company</span>
+                </a>
             </li>
     </aside>
 </div>
