dd5862f0f32f9dca6cdf00b878c8e97b20ac4839
fiisabilillah <officialteam548@gmail.com>
2024-07-17 03:05:20 +0700

    percobaan api

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+// login
+Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
