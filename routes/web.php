fdca0559f8f4b7950648d0e5e1a5589899a97b3a
fiisabilillah <officialteam548@gmail.com>
2024-07-16 07:29:10 +0700

    fortify, login, logout

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/dashboard', function () {
-    return view('pages.dashboard', ['type_menu' => 'dashboard']);
+Route::get('/', function () {
+    return view('pages.auth.auth-login');
 });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
+
+    // Route::resource('users', UserController::class);
+    // Route::resource('companies', CompanyController::class);
+    // Route::resource('attendances', AttendanceController::class);
+    // Route::resource('permissions', PermissionController::class);
+});
+
+
