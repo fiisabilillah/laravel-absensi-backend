f5675721b973bc99d1763cddd72490907910921e
fiisabilillah <officialteam548@gmail.com>
2024-08-07 03:01:10 +0700

    FIC16 Zoom8 menit:02:07:58 add added modified controller config

--- app/Http/Controllers/Api/PermissionController.php
+++ app/Http/Controllers/Api/PermissionController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Permission;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Kreait\Firebase\Messaging\CloudMessage;
+use Kreait\Firebase\Messaging\Notification;
 
 class PermissionController extends Controller
 {
@@ -31,5 +34,30 @@ class PermissionController extends Controller
         $permission->save();
 
         return response()->json(['message' => 'Permission created successfully'], 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $permission = Permission::find($id);
+        $permission->is_approved = $request->is_approved;
+        $str = $request->is_approved == 1 ? 'Disetujui' : 'Ditolak';
+        $permission->save();
+        $this->sendNotificationToUser($permission->user_id, 'Status izin anda adalah' . $str);
+        return redirect()->route('permissions.index')->with('success', 'Permission updated successfully');
+    }
+
+    public function sendNotificationToUser($userId, $mesage)
+    {
+        // dapatkan FCM
+        $user = User::find($userId);
+        $token = $user->fcm_token;
+
+        //kirim notifikasi ke perangkat android
+        $messagging = app('firebase.messaging');
+        $notification = Notification::create('Status Izin', $mesage);
+
+        $mesage = CloudMessage::withTarget('token', $token)
+            ->withNotification($notification);
+        $messagging->send($mesage);
     }
 }
